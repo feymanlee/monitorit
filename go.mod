666dbd9e5f390d8249a3545ae3b4965f2d70751a
feyman <admin@feyman.me>
2023-08-28 19:59:42 +0800

    add xorm stat

--- go.mod
+++ go.mod
@@ -16,17 +16,25 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-playground/assert/v2 v2.2.0 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/goccy/go-json v0.8.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
 	github.com/prometheus/common v0.44.0 // indirect
 	github.com/prometheus/procfs v0.10.1 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230629202037-9506855d4529 // indirect
 	google.golang.org/grpc v1.56.1 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
+	xorm.io/xorm v1.3.2 // indirect
 )
---
+++ xorm/hook.go
@@ -0,0 +1,86 @@
+//
+// Package xorm
+// @Author: feymanlee@gmail.com
+// @Description:
+// @File:  hook
+// @Date: 2023/8/28 19:25
+//
+
+package xorm
+
+import (
+	"context"
+	"strings"
+
+	"github.com/feymanlee/monitorit"
+	"github.com/prometheus/client_golang/prometheus"
+	"xorm.io/xorm/contexts"
+)
+
+type Hook struct {
+	options        *Options
+	instanceName   string
+	queryHistogram *prometheus.HistogramVec
+	queryCounter   *prometheus.CounterVec
+	errorCounter   *prometheus.CounterVec
+}
+
+var (
+	queryLabelNames = []string{"db_name", "command"}
+	errorLabelNames = []string{"db_name", "command", "error"}
+)
+
+func NewHook(dbName string, opts ...Option) *Hook {
+	options := DefaultOptions()
+	options.Merge(opts...)
+	c := Hook{
+		options:      options,
+		instanceName: dbName,
+	}
+	c.queryHistogram = monitorit.Register(prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: options.Namespace,
+		Subsystem: options.Subsystem,
+		Name:      "query_duration_sec",
+		Help:      "Histogram of GORM query duration in seconds",
+		Buckets:   options.DurationBuckets,
+	}, queryLabelNames)).(*prometheus.HistogramVec)
+
+	c.queryCounter = monitorit.Register(prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: options.Namespace,
+		Subsystem: options.Subsystem,
+		Name:      "query_total",
+		Help:      "Number of GORM queries total",
+	}, queryLabelNames)).(*prometheus.CounterVec)
+
+	c.errorCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "query_err_total",
+			Help: "Total number of GORM query errors",
+		},
+		errorLabelNames,
+	)
+	return &c
+}
+
+func (h *Hook) BeforeProcess(c *contexts.ContextHook) (context.Context, error) {
+	return c.Ctx, nil
+}
+
+func (h *Hook) AfterProcess(c *contexts.ContextHook) error {
+	queryType := h.getQueryType(c.SQL)
+	h.queryCounter.WithLabelValues(h.instanceName, queryType).Inc()
+	h.queryHistogram.WithLabelValues(h.instanceName, queryType).Observe(c.ExecuteTime.Seconds())
+	if c.Err != nil {
+		h.errorCounter.WithLabelValues(h.instanceName, queryType, c.Err.Error()).Inc()
+	}
+	return nil
+}
+
+func (h *Hook) getQueryType(sql string) string {
+	index := strings.Index(sql, " ")
+	if index != -1 {
+		return sql[:index]
+	} else {
+		return "unknown"
+	}
+}
